f3c54d3f19e0189b8fdb88116e6a2c7e5e4d86c9
India Derrick <indiaderrick@gmail.com>
2019-01-08 17:26:53 +0000

    added skills and fieldset to projects

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 window.onload = () => {
   const burger = document.querySelector('.burger');
   const projects = document.querySelector('.projects');
+  const technologiesText = document.querySelector('.technologies-text');
+  const descriptionText = document.querySelector('.description-text');
+  const technologiesButton = document.querySelector('.technologies-button');
+  const descriptionButton = document.querySelector('.description-button');
 
   const welcomeInView = () => {
     const projectsInView = projects.getBoundingClientRect();
@@ -11,13 +15,31 @@ window.onload = () => {
     }
   };
 
+  technologiesButton.addEventListener('click', () => {
+    technologiesText.classList.add('show-technologies');
+    technologiesButton.classList.add('white');
+    descriptionButton.classList.remove('white');
+    descriptionText.classList.add('hide-description');
+  });
+
+  descriptionButton.addEventListener('click', () => {
+    technologiesText.classList.remove('show-technologies');
+    technologiesButton.classList.remove('white');
+    descriptionButton.classList.add('white');
+    descriptionText.classList.remove('hide-description');
+  });
+
+
   window.addEventListener('scroll', () => {
     if (welcomeInView()) {
       console.log('doesnt need navburger');
       burger.classList.remove('burger-show');
+      // navAbout.classList.remove('burger-show');
     } else {
       console.log('needs navburger');
       burger.classList.add('burger-show');
+      // navAbout.classList.add('burger-show');
     }
   });
+  //look up how to do a hoverable nav bar with js css and html
 };
